93b867dcf59a6a1b664b940dbbeff3097ef6dac8
qff233 <347633553@qq.com>
2023-02-28 22:13:39 +0800

    修改vendor

---
+++ server/application.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Qff on 2023/2/28.
+//
+
+#include "application.h"
+
+namespace auv
+{
+
+}
---
+++ server/application.h
@@ -0,0 +1,28 @@
+//
+// Created by Qff on 2023/2/28.
+//
+
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+#include <thread>
+#include <map>
+#include <functional>
+
+namespace auv
+{
+
+class Application
+{
+public:
+
+	void reload();
+
+private:
+	
+};
+
+}
+
+
+#endif //APPLICATION_H
