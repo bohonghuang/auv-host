838e887b2ecdd9e5e702efc166f456c5f7d7aacd
qff233 <347633553@qq.com>
2023-03-17 19:51:15 +0800

    添加 `imshow.h`

---
+++ vision/block/imshow.h
@@ -0,0 +1,22 @@
+//
+// Created by qff233 on 23-3-17.
+//
+
+#ifndef AUV_HOST_IMSHOW_H
+#define AUV_HOST_IMSHOW_H
+
+#include <opencv2/opencv.hpp>
+
+#include "block.h"
+
+namespace auv::vision {
+
+class ImshowBlock : public Block<cv::Mat, std::tuple<>> {
+public:
+  Out process(In) noexcept override;
+  AUV_BLOCK;
+};
+
+}
+
+#endif//AUV_HOST_IMSHOW_H
