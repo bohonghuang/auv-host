a80d7d54a0ecadd01b449eb0ca33f763968c0c54
coco24 <1281299809@qq.com>
2023-03-19 20:06:46 +0800

    添加宏 `AUV_NEW_SOL_TYPE`

--- runtime/lua_interop.h
+++ runtime/lua_interop.h
@@ -9,6 +9,29 @@
 
 namespace auv::lua {
 
+template<class T>
+constexpr static auto object_to_any_function = [](T obj) -> std::any { return obj; };
+
+template<class T>
+constexpr static auto object_from_any_function = [](std::any obj) -> T { return std::any_cast<T>(obj); };
+
+constexpr const char *type_name_without_namespace(const char *name) {
+  const char *result = name;
+  char prev_char = '\0';
+  while (*name != '\0') {
+    if (*name == ':' && prev_char == ':') result = name + 1;
+    prev_char = *name++;
+  }
+  return result;
+}
+
+#define AUV_NEW_SOL_TYPE(TABLE, TYPE, ...)                                                                      \
+  {                                                                                                             \
+    TABLE.new_usertype<TYPE>(auv::lua::type_name_without_namespace(#TYPE), ##__VA_ARGS__);                      \
+    TABLE[auv::lua::type_name_without_namespace(#TYPE)]["from_any"] = auv::lua::object_from_any_function<TYPE>; \
+    TABLE[auv::lua::type_name_without_namespace(#TYPE)]["to_any"] = auv::lua::object_to_any_function<TYPE>;     \
+  }
+
 template<class T, class = decltype(&T::process), class = void>
 struct block_process_overload_impl {
   static auto get() {
